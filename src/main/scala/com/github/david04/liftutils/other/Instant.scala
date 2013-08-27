b55b88971b74c61f7359851ea7697e40fd6a0b75
David Antunes <davidmiguel@antunes.net>
2013-08-27 12:47:25 +0100

    Added new instant select box.

--- src/main/scala/com/github/david04/liftutils/other/Instant.scala
+++ src/main/scala/com/github/david04/liftutils/other/Instant.scala
@@ -1,13 +1,14 @@
 package com.github.david04.liftutils.other
 
 import net.liftweb.http.SHtml.ElemAttr
-import scala.xml.NodeSeq
+import scala.xml.{Text, NodeSeq}
 import java.util.UUID
 import net.liftweb.http.SHtml
 import net.liftweb.http.js.JE.{JsNull, JsRaw}
 import net.liftweb.json._
 import net.liftweb.http.js.JsCmds.{Replace, SetHtml}
 import net.liftweb.http.js.JsCmd
+import com.github.david04.liftutils.util.Util
 
 
 object InstantEdit {
@@ -32,6 +33,26 @@ object InstantEdit {
     display
   }
 
+  def select[T](get: () => T, set: (T) => Unit, all: Seq[T], toStr: T => String, attrs: ElemAttr*): NodeSeq = {
+    val uid = UUID.randomUUID().toString
+    attrs.foldLeft(<select id={uid} onclick={SHtml.jsonCall(JsRaw("$(this).val()"), (v: JValue) => v match {
+      case v: JString => {
+        all.find(_.## == v.s.toInt).foreach(v => set(v))
+        SetHtml(uid, select(get, set, all , toStr, attrs: _*))
+      }
+    }).toJsCmd}>
+      {all.toSeq.map(v => if (v == get())
+        <option selected="true" value={v.## + ""}>
+          {toStr(v)} qwdqwd
+        </option>
+      else
+        <option value={v.## + ""}>
+          {toStr(v)}qwdqwd
+        </option>
+      )}
+    </select>)(_ % _)
+  }
+
   def enum[ENUM <: Enumeration](e: ENUM, get: () => ENUM#Value, set: (ENUM#Value) => Unit, attrs: ElemAttr*): NodeSeq = {
     val uid = UUID.randomUUID().toString
     attrs.foldLeft(<select id={uid} onclick={SHtml.jsonCall(JsRaw("$(this).val()"), (v: JValue) => v match {
@@ -50,6 +71,24 @@ object InstantEdit {
         </option>
       )}
     </select>)(_ % _)
+  }
+
+  def enumNav[ENUM <: Enumeration](e: ENUM, get: () => ENUM#Value, set: (ENUM#Value) => JsCmd, attrs: ElemAttr*): NodeSeq = {
+    val id = UUID.randomUUID().toString
+    <ul class="nav" id={id}>
+      <li class="dropdown">
+        <a data-toggle="dropdown" class="dropdown-toggle" href="#" id={id + "head"}>
+          {get().toString}
+        </a>
+        <ul class="dropdown-menu">
+          {e.values.map(v => <li>
+          <a href="#" onclick={Util.run({set(v) & SetHtml(id + "head", Text(v.toString))})}>
+            {v.toString}
+          </a>
+        </li>)}
+        </ul>
+      </li>
+    </ul>
   }
 
   def checkbox(caption: () => String, get: () => Boolean, set: (Boolean) => JsCmd, attrs: ElemAttr*): NodeSeq = {
